d0104579339cb0c04849a1b3912434ed90ecaaab
Orhan ULUTAŞ <116510623+OrhanULUTAS@users.noreply.github.com>
2023-12-12 19:45:37 +0300

    Update ornek.py

--- ornek.py
+++ ornek.py
@@ -2,8 +2,10 @@ from flask import Flask, request
 from flask_restful import Api, Resource
 import pandas as pd
 
+
 app = Flask(__name__)
 api = Api(app)
+
 
 class Users(Resource):
     def get(self):
