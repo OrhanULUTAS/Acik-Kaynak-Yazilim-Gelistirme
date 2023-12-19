c46b99935c1c198bd31d715597492361a158a8bb
Orhan ULUTAŞ <116510623+OrhanULUTAS@users.noreply.github.com>
2023-12-20 02:43:26 +0300

    Add files via upload

--- Flask.py
+++ Flask.py
@@ -1,9 +1,9 @@
-  GNU nano 6.2                             /home/ubuntu/myproject/myproject.py                                       from flask import Flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Dogru Bir Sekilde Calısıyor</h1>"
+    return "<h1 style='color:blue'>Hello There!</h1>"
 
 if __name__ == "__main__":
       app.run(host="0.0.0.0", port=6767)
